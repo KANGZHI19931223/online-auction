6e0f4c8128766b9b58d86d38c7e2462b46331cea
KANGZHI19931223 <xk88881@163.com>
2019-06-27 17:34:55 +0800

    基本组件完成

--- src/app/stars/stars.component.ts
+++ src/app/stars/stars.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-stars',
@@ -7,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StarsComponent implements OnInit {
 
+  private stars: boolean[];
+
+  // 接收 product 组件传递过来的星级评价的数值
+  @Input() private rating: number = 0;
+
   constructor() { }
 
   ngOnInit() {
+
+    this.stars = [];
+
+    for (let i = 1; i <= 5; i++) {
+
+      this.stars.push(i > this.rating);
+
+    }
+
   }
 
 }
